8129d443098ec12e452f104060d7dbcc2b961c0f
CircleCI <ryan+circle-vip-go@hmn.md>
2017-11-09 06:35:08 +0000

    Build changes from b9b8090e570c43de2598bae4295552c603605fd0
    https://circleci.com/gh/humanmade/vip-go-builder/5

---
+++ .circleci/deploy.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -e
+#
+# Deploy your branch on VIP Go.
+#
+
+BRANCH="${CIRCLE_BRANCH}"
+DEPLOY_SUFFIX="${DEPLOY_SUFFIX:--built}"
+SRC_DIR="$PWD"
+BUILD_DIR="/tmp/vip-go-build"
+
+if [[ -z "$BRANCH" ]]; then
+	echo "No branch specified!"
+	exit 1
+fi
+
+if [[ -d "$BUILD_DIR" ]]; then
+	echo "WARNING: ${BUILD_DIR} already exists. You may have accidentally cached this"
+	echo "directory. This will cause issues with deploying."
+	exit 1
+fi
+
+COMMIT=$(git rev-parse HEAD)
+DEPLOY_BRANCH="${BRANCH}${DEPLOY_SUFFIX}"
+echo "Deploying $BRANCH to $DEPLOY_BRANCH"
+
+# If the deploy branch doesn't already exist, create it from the empty root.
+if ! git rev-parse --verify "$DEPLOY_BRANCH" >/dev/null 2>&1; then
+	echo -e "\nCreating $DEPLOY_BRANCH..."
+	git worktree add --detach "$BUILD_DIR"
+	cd "$BUILD_DIR"
+	git checkout --orphan "$DEPLOY_BRANCH"
+else
+	echo "Using existing $DEPLOY_BRANCH"
+	git worktree add "$BUILD_DIR" "$DEPLOY_BRANCH"
+fi
+
+# Ensure we're in the right dir
+cd "$BUILD_DIR"
+
+# Remove existing files
+git rm -rfq .
+
+# Sync built files
+echo -e "\nSyncing files..."
+if ! command -v 'rsync'; then
+	sudo apt-get install -q -y rsync
+fi
+
+rsync -av "$SRC_DIR/" "$BUILD_DIR" --exclude ".git"
+
+# Add changed files
+git add .
+
+if [ -z "$(git status --porcelain)" ]; then
+	echo "No changes to built files."
+	exit
+fi
+
+# Print status!
+echo -e "\nSynced files. Changed:"
+git status -s
+
+# Double-check our user/email config
+if ! git config user.email; then
+	git config user.name "CircleCI"
+	git config user.email "ryan+circle-vip-go@hmn.md"
+fi
+
+# Commit it.
+MESSAGE=$( printf 'Build changes from %s\n\n%s' "${COMMIT}" "${CIRCLE_BUILD_URL}" )
+git commit -m "$MESSAGE"
+
+# Push it (real good).
+git push origin "$DEPLOY_BRANCH"
